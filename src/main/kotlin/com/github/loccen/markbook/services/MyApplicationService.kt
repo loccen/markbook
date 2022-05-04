66e2d5d14d848a8c9bf21e0b3ae7fcec9e67c134
GitHub Action <action@github.com>
2022-05-04 05:26:47 +0000

    Template cleanup

--- src/main/kotlin/com/github/loccen/markbook/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/loccen/markbook/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.loccen.markbook.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.loccen.markbook.MyBundle
 
 class MyApplicationService {
 
